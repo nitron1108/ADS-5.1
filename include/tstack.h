8fc92bdfa82133c800f80260cb9bebae6d286717
nitron1108 <mja29122@gmail.com>
2026-06-05 22:49:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,48 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <stdexcept>
+
+template<typename ElementType, int Capacity>
 class TStack {
-  // добавьте код стека
+ public:
+  TStack(){
+    topIndex = -1;
+  }
+
+  bool isVoid() const {
+    return topIndex == -1;
+  }
+
+  bool isFull() const {
+    return topIndex == Capacity - 1;
+  }
+
+  void add(const ElementType& value) {
+    if (isFull()) {
+      throw std::overflow_error("overflow");
+    }
+    data[++topIndex] = value;
+  }
+
+  ElementType remove() {
+    if (isVoid()) {
+      throw std::underflow_error("underflow");
+    }
+    return data[topIndex--];
+  }
+
+  ElementType getTop() const {
+    if (isVoid()) {
+      throw std::underflow_error("empty stack ");
+    }
+    return data[topIndex];
+  }
+
+ private:
+  ElementType data[Capacity];
+  int topIndex;
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif  
