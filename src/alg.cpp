a81ef81ba926894b0948cb0b0d7fe721055b439a
nitron1108 <mja29122@gmail.com>
2026-06-05 22:48:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,48 @@
 // Copyright 2025 NNTU-CS
-#include <string>
-#include <map>
-#include "tstack.h"
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
-}
+#include <stdexcept>
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
-}
+template<typename ElementType, int Capacity>
+class TStack {
+ public:
+  TStack(){
+    topIndex = -1;
+  }
+
+  bool isVoid() const {
+    return topIndex == -1;
+  }
+
+  bool isFull() const {
+    return topIndex == Capacity - 1;
+  }
+
+  void add(const ElementType& value) {
+    if (isFull()) {
+      throw std::overflow_error("overflow");
+    }
+    data[++topIndex] = value;
+  }
+
+  ElementType remove() {
+    if (isVoid()) {
+      throw std::underflow_error("underflow");
+    }
+    return data[topIndex--];
+  }
+
+  ElementType getTop() const {
+    if (isVoid()) {
+      throw std::underflow_error("empty stack ");
+    }
+    return data[topIndex];
+  }
+
+ private:
+  ElementType data[Capacity];
+  int topIndex;
+};
+
+#endif  
